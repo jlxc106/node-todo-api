c9fbd151bd8f9b7c439a74ad723415c332f3c8b5
jlxc106 <jaylim106@gmail.com>
2018-05-11 17:12:44 -0700

    add post /users/login

--- server/models/user.js
+++ server/models/user.js
@@ -67,6 +67,24 @@ UserSchema.statics.findByToken = function(token){   //model methods get called w
     })
 }
 
+UserSchema.statics.findByCredentials = function (email, password){
+    var User = this;
+    return User.findOne({email}).then((user =>{
+        if(!user){
+            return Promise.reject();
+        }
+        return new Promise((resolve, reject) =>{
+            bcrypt.compare(password, user.password, (err, res)=>{
+                if(res){
+                    resolve(user);
+                }else{
+                    reject();
+                }
+            })
+        })
+    }))
+};
+
 //is called before save
 UserSchema.pre('save', function(next){
     var user = this;
